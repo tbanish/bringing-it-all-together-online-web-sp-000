16160a97ba64e6c4b29a1fd6fdc6d519b547caf7
Tom Banish <tombanish@gmail.com>
2020-06-10 05:01:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -80,9 +80,11 @@ class Dog
     sql = <<-SQL
       SELECT * FROM dogs
       WHERE name = ?
+      LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, name).map do 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new()
     
   end
   
